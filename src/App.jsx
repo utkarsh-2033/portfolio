b8bb407902e10a3f86562801670f4e525f6b5b31
utkarsh-2033 <utkarshg@2033>
2025-08-01 22:23:59 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   const [sidebarActive, setSidebarActive] = useState(false);
   const handleSidebarToggle = () => setSidebarActive((prev) => !prev);
   return (
     <main>
-      <Analytics/>
       <aside className={`sidebar${sidebarActive ? ' active' : ''}`}>
         <Sidebar onSidebarToggle={handleSidebarToggle} sidebarActive={sidebarActive} />
       </aside>
       <div className="main-content">
         <MainContent />
       </div>
+      <Analytics />
     </main>
   );
 }
